c252af91700060eff8190c72eded82ac29c29af4
jaceklaskowski <jaceklaskowski>
2022-01-24 17:58:32 +0000

    Advanced Spark SQL + Delta Lake

---
+++ 04_Delta Lake.sql
@@ -0,0 +1,66 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC # Delta Lake
+-- MAGIC 
+-- MAGIC by Jacek Laskowski (jacek@japila.pl)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC ## Welcome
+-- MAGIC 
+-- MAGIC This is the last module (out of 4) to teach you how to use and think like a Spark SQL and Delta Lake pro.
+-- MAGIC 
+-- MAGIC 1 module takes 1,5h (2 x 45 mins with no break in-between)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC ## Schedule
+-- MAGIC 
+-- MAGIC ### Module 1. The Essentials of Spark SQL
+-- MAGIC 
+-- MAGIC * Part 1
+-- MAGIC   * Databricks Platform
+-- MAGIC   * Loading and Saving Datasets
+-- MAGIC * Part 2
+-- MAGIC   * Basic Transformations
+-- MAGIC   * Web UI
+-- MAGIC   
+-- MAGIC ### Module 2. Intermediate Spark SQL
+-- MAGIC 
+-- MAGIC * Part 1
+-- MAGIC   * Aggregations and Joins
+-- MAGIC * Part 2
+-- MAGIC   * [Data Sources](https://spark.apache.org/docs/latest/sql-data-sources.html)
+-- MAGIC   * Loading Datasets from Cloud Storage
+-- MAGIC 
+-- MAGIC ### Module 3. Advanced Spark SQL
+-- MAGIC 
+-- MAGIC * Part 1
+-- MAGIC   * Windowed Aggregation
+-- MAGIC * Part 2
+-- MAGIC   * Caching and Persistence
+-- MAGIC   * The Internals of Structured Query Execution
+-- MAGIC 
+-- MAGIC ### Module 4. Delta Lake (this notebook)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC ## Learning Resources
+-- MAGIC 
+-- MAGIC The recommended learning resources (for reading and watching) to get better equipped for the Spark Enablement series:
+-- MAGIC 
+-- MAGIC * [Apache Spark](https://spark.apache.org)
+-- MAGIC * [Delta Lake](https://delta.io)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC # Delta Lake
